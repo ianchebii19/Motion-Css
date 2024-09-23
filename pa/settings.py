161f9fe0cc607710c19e6a932ec20887595475fe
Ian Chebii <ianchebii19@gmail.com>
2024-09-23 23:03:21 +0300

    keimo

--- pa/settings.py
+++ pa/settings.py
@@ -113,8 +113,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [BASE_DIR/"static"]
-# Default primary key field type
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
